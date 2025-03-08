adf81b0a697f291fc0a04f4c5e2d58e30f48d1fb
Vanderson Dill <vanderson.dill@townsq.com.br>
2025-03-08 10:10:33 -0300

    chore(wksp): add spartan-ui components

--- apps/fin-web/src/app/app.component.ts
+++ apps/fin-web/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { UiComponent } from '@fin-web/ui';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
-  imports: [UiComponent, RouterModule],
+  imports: [RouterModule, HlmButtonDirective],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ libs/ui/ui-datepicker-helm/README.md
@@ -0,0 +1,7 @@
+# ui-datepicker-helm
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ui-datepicker-helm` to execute the unit tests.
