91e72cc72d17933d08d5471f92c4ea74379e1da5
Vanderson Dill <vanderson.dill@townsq.com.br>
2025-03-08 09:50:17 -0300

    refactor(fin-web): refactor test to reflect changes in the libs

--- apps/fin-web/src/app/app.component.spec.ts
+++ apps/fin-web/src/app/app.component.spec.ts
@@ -1,23 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { UiComponent } from '@fin-web/ui';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      imports: [AppComponent, UiComponent, RouterModule.forRoot([])],
     }).compileComponents();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome fin-web'
-    );
-  });
 
   it(`should have as title 'fin-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
