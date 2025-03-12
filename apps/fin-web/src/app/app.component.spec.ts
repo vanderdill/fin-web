7bd0adc1cf0ee73023293c83821feae0404355dd
Vanderson Dill <vanderson.dill@townsq.com.br>
2025-03-11 22:56:57 -0300

    test(biz): remove wrong import

--- apps/fin-web/src/app/app.component.spec.ts
+++ apps/fin-web/src/app/app.component.spec.ts
@@ -1,15 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { UiComponent } from '@fin-web/ui';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, UiComponent, RouterModule.forRoot([])],
+      imports: [AppComponent, RouterModule.forRoot([])],
     }).compileComponents();
   });
-
 
   it(`should have as title 'fin-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
